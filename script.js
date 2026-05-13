445396fa4205aaafebaa6698ef36b4d4edf119a1
polaronn2-lab <polaronn.2@gmail.com>
2026-05-14 00:54:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const letters = {
   "mathwizard": {
     img: "zachzach.jpg",
-    color: "#f5f5dc",
+    color: "#d2b48c",
     text: `
 HI PREEEE
 
-So Happy grad parechong!! Medjo I don't know how to be sentimental and all that but I want to say congratulations pree like goddamn hard carry sobra HAHAHAHHAHA Honestly though, you were one of the first that I knew I would be classmates 
-sa Humility, and then the rest is history na. Never expected us to be the beadle-sub beadle duo but hey, I know we did a great job because Humility reflects that. Honestly wouldn't know how I'd handle the class if hindi ko pinapasa sayo 
-yung announcements na ayaw ko isigaw HAHAHAHAHA and jusko don't get me started sa physics away natin, honestly would miss those. or kung makikihiram ka lang ng ipad for a bangs update, or if di sila nakikinig sakin kaya mas malakas ng sigaw mo.
-They were moments na wala, very simple, yet it felt like they made us more like this 🤞🤞🤞 I don't know, but I'm not a very vocal person with appreciation, so I always tried to be there naman. In quiet or gloomy days, I'm glad I had a katabi, a seatmate
-that was there. Thanks for everything pree, we may be kilometers apart but I'm a message away ah, mag uupdate pa tayong apat sa row alr? Congrats uli sa grad! Padayon future archi!! 
+So Happy grad parechong!! Medjo I don't know how to be sentimental and all that but I want to say congratulations pree like goddamn hard carry sobra HAHAHAHHAHA Honestly though, you were one of the first that I knew I would be classmates  sa Humility, and then the rest is history na. Never expected us to be the beadle-sub beadle duo but hey, I know we did a great job because Humility reflects that. Honestly wouldn't know how I'd handle the class if hindi ko pinapasa sayo  yung announcements na ayaw ko isigaw HAHAHAHAHA and jusko don't get me started sa physics away natin, honestly would miss those. or kung makikihiram ka lang ng ipad for a bangs update, or if di sila nakikinig sakin kaya mas malakas ng sigaw mo. They were moments na wala, very simple, yet it felt like they made us more like this 🤞🤞🤞 I don't know, but I'm not a very vocal person with appreciation, so I always tried to be there naman. In quiet or gloomy days, I'm glad I had a katabi, a seatmate that was there. Thanks for everything pree, we may be kilometers apart but I'm a message away ah, mag uupdate pa tayong apat sa row alr? Congrats uli sa grad! Padayon future archi!! 
     `
   },
 
