f563d4c0a08f3729eee8964b6449b042965966a1
polaronn2-lab <polaronn.2@gmail.com>
2026-05-14 01:18:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,63 +34,6 @@ function checkPassword() {
   const input = document.getElementById("password").value;
   const letterBox = document.getElementById("letterBox");
 
-  if (letters[input]) {
-    const data = letters[input];
-
-    letterBox.innerHTML = `
-      <div class="card" style="background:${data.color}">
-        <img src="${data.img}" class="friend-img">
-        <div class="letter">
-          ${data.text.replace(/\n/g, "<br>")}
-        </div>
-      </div>
-    `;
-  } else {
-    letterBox.innerHTML = `<p class="error">Wrong password.</p>`;
-  }
-}
-const letters = {
-  "mathwizard": {
-    img: "zachzach.jpg",
-    color: "#d2b48c",
-    song: "zachzach.mp3",
-    decor: ["zach1.png", "zach2.png"],
-    text: `
-HI PREEEE
-
-So Happy grad parechong!! Medjo I don't know how to be sentimental and all that but I want to say congratulations pree like goddamn hard carry sobra HAHAHAHHAHA Honestly though, you were one of the first that I knew I would be classmates  sa Humility, and then the rest is history na. Never expected us to be the beadle-sub beadle duo but hey, I know we did a great job because Humility reflects that. Honestly wouldn't know how I'd handle the class if hindi ko pinapasa sayo  yung announcements na ayaw ko isigaw HAHAHAHAHA and jusko don't get me started sa physics away natin, honestly would miss those. or kung makikihiram ka lang ng ipad for a bangs update, or if di sila nakikinig sakin kaya mas malakas ng sigaw mo. They were moments na wala, very simple, yet it felt like they made us more like this 🤞🤞🤞 I don't know, but I'm not a very vocal person with appreciation, so I always tried to be there naman. In quiet or gloomy days, I'm glad I had a katabi, a seatmate that was there. Thanks for everything pree, we may be kilometers apart but I'm a message away ah, mag uupdate pa tayong apat sa row alr? Congrats uli sa grad! Padayon future archi!! 
-    `
-  },
-
-  "seiji123": {
-    img: "seiji.jpg",
-    color: "#e1f0ff",
-    song: "seiji.mp3",
-    decor: ["cloud1.png", "cloud2.png"],
-    text: `
-Dear Seiji,
-
-You made everything lighter and more memorable.
-    `
-  },
-
-  "vince123": {
-    img: "vince.jpg",
-    color: "#fff1dc",
-    song: "vince.mp3",
-    decor: ["sparkle1.png", "sparkle2.png"],
-    text: `
-Dear Vince,
-
-I’ll always remember the moments we shared.
-    `
-  }
-};
-
-function checkPassword() {
-  const input = document.getElementById("password").value;
-  const letterBox = document.getElementById("letterBox");
-
   if (!letters[input]) {
     letterBox.innerHTML = `<p class="error">Wrong password.</p>`;
     return;
