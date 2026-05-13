18211cd9388d4eda8262a9e6a5c9a7d9ebf4582e
polaronn2-lab <polaronn.2@gmail.com>
2026-05-14 01:23:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const letters = {
   "mathwizard": {
     img: "zachzach.jpg",
     color: "#d2b48c",
+    song: "zachzach.mp3",
+    decor: ["zach1.png", "zach2.png"],
     text: `
 HI PREEEE
 
@@ -12,6 +14,8 @@ So Happy grad parechong!! Medjo I don't know how to be sentimental and all that 
   "seiji123": {
     img: "seiji.jpg",
     color: "#e1f0ff",
+    song: "seiji.mp3",
+    decor: ["cloud1.png", "cloud2.png"],
     text: `
 Dear Seiji,
 
@@ -22,6 +26,8 @@ You made everything lighter and more memorable.
   "vince123": {
     img: "vince.jpg",
     color: "#fff1dc",
+    song: "vince.mp3",
+    decor: ["sparkle1.png", "sparkle2.png"],
     text: `
 Dear Vince,
 
@@ -29,57 +35,3 @@ I’ll always remember the moments we shared.
     `
   }
 };
-
-function checkPassword() {
-  const input = document.getElementById("password").value;
-  const letterBox = document.getElementById("letterBox");
-
-  if (!letters[input]) {
-    letterBox.innerHTML = `<p class="error">Wrong password.</p>`;
-    return;
-  }
-
-  const data = letters[input];
-
-  letterBox.innerHTML = `
-    <div class="scene">
-
-      <img src="${data.decor[0]}" class="floating left">
-      <img src="${data.decor[1]}" class="floating right">
-
-      <div class="card hidden" id="card" style="background:${data.color}">
-
-        <img src="${data.img}" class="friend-img hidden" id="img">
-
-        <div class="letter hidden" id="text">
-          ${data.text.replace(/\n/g, "<br>")}
-        </div>
-
-        <audio id="bgmusic" src="${data.song}"></audio>
-
-      </div>
-
-    </div>
-  `;
-
-  // STEP 1: show card
-  setTimeout(() => {
-    document.getElementById("card").classList.add("show");
-  }, 100);
-
-  // STEP 2: image fade in
-  setTimeout(() => {
-    document.getElementById("img").classList.add("show");
-  }, 600);
-
-  // STEP 3: text fade in
-  setTimeout(() => {
-    document.getElementById("text").classList.add("show");
-  }, 1200);
-
-  // STEP 4: play music
-  setTimeout(() => {
-    const music = document.getElementById("bgmusic");
-    music.play();
-  }, 800);
-}
