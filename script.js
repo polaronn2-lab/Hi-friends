7622da754a412004a51d8438c4223fbe4034e627
polaronn2-lab <polaronn.2@gmail.com>
2026-05-13 23:04:48 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const letters = {
+  "juvi123": `
+Dear Juvi,
+
+Thank you for always being there...
+`,
+
+  "seiji123": `
+Dear Seiji,
+
+I appreciate our friendship...
+`,
+
+  "vince123": `
+Dear Vince,
+
+You’ve made life more memorable...
+`
+};
+
+function checkPassword() {
+  const input = document.getElementById("password").value;
+  const letterBox = document.getElementById("letterBox");
+
+  if (letters[input]) {
+    letterBox.innerText = letters[input];
+  } else {
+    letterBox.innerText = "Wrong password.";
+  }
+}
